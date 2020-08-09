efc131f24ace1652de0b4c663aa7520b2af37199
Peter Lau <superpeterlau@outlook.com>
2020-08-09 17:48:09 +0800

    ruby: String

--- ruby/tour/13.rb
+++ ruby/tour/13.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/ruby -w
+
+content = String.new "some new words"
+
+puts content.downcase
+puts content.upcase
+puts content
+
+puts "insert #{content}"
+
+puts "whole".unpack "xax2aX2aX1aX2a"
