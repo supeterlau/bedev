5b3f0343f807a002a5164366f5649f6a360d8526
Peter Lau <superpeterlau@outlook.com>
2020-08-10 23:43:59 +0800

    ruby: regular expression

--- ruby/tour/22.rb
+++ ruby/tour/22.rb
@@ -10,3 +10,15 @@ end
 if line2 =~ /dogs(.*)/
   puts "line2 contains dog"
 end
+
+text = "rails are rails, really good Ruby on Rails"
+
+text.gsub!('rails', 'Rails')
+
+puts text
+
+text = "rails are rails, really good Ruby on Rails"
+
+text.gsub!(/\brails\b/, 'Rails')
+
+puts text
