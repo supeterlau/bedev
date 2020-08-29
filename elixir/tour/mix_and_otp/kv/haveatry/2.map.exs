418debf248ac3683994ff7f6b6cd5a545a0c078b
Peter Lau <superpeterlau@outlook.com>
2020-08-29 18:48:50 +0800

    elixir: mix_and_otp ch7

--- elixir/tour/mix_and_otp/kv/haveatry/2.map.exs
+++
@@ -1,7 +0,0 @@
-map = %{}
-
-map |> Map.get(:noop) |> IO.inspect
-
-Map.put(map, :new_key, "new value") |> IO.inspect
-
-Map.pop(map, :new_key) |> IO.inspect
--- elixir/tour/mix_and_otp/kv/haveatry/3.map.exs
+++
@@ -1,7 +0,0 @@
-defmodule Tour do
-  def sigil_m(value) do
-    quote do: value
-  end
-end
-
-# ~m(aa) == %{aa: aa}
