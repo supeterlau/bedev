013be98578238dcd547c791494c23db0fa344820
Peter Lau <superpeterlau@outlook.com>
2020-09-05 20:05:27 +0800

    clojure: braveclojure ch1,ch2

---
+++ clojure/tour/cat_noob/src/cat_noob/core.clj
@@ -0,0 +1,17 @@
+(ns cat-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Angry teapot!"))
+
+(println "Cleanliness is next to godliness")
+
+(defn train
+[]
+(println "Choo choo!"))
+
+(map inc [1 2 4 5])
+
+(+ 1 (* 2 3 4 5) 6)
