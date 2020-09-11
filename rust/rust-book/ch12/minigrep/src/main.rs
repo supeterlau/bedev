3f3ff51a8d06c41efa10345aaff586d493a8a8af
Peter Lau <superpeterlau@outlook.com>
2020-09-11 14:23:40 +0800

    rust: rust-book ch12 read file

--- rust/rust-book/ch12/minigrep/src/main.rs
+++ rust/rust-book/ch12/minigrep/src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
+use std::fs;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  println!("Got {:?}", args);
+  // println!("Got {:?}", args);
+  let query = &args[1];
+  let filename = &args[2];
+
+  println!("Searching for {}", query);
+  println!("In file {}", filename);
+
+  // let hello = String::from("Hello");
+  // println!("{}", format!("- {}", hello.as_str()));
+  // panic!(format!("- {}", filename));
+
+  let contents = fs::read_to_string(filename)
+    .expect(format!("Something went wrong reading the file: {}", *filename).as_str());
+  println!("With text:\n{}", contents);
 }
