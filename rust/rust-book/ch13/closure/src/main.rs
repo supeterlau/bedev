d45c5c2a2caf94c992ad737705b195b8e9eb6bb0
Peter Lau <superpeterlau@outlook.com>
2020-09-25 16:56:33 +0800

    rust: rust-book ch13 closure : Capturing the Environment with Closures

--- rust/rust-book/ch13/closure/src/main.rs
+++ rust/rust-book/ch13/closure/src/main.rs
@@ -40,6 +40,17 @@ fn main() {
 
 
   generate_workout(simulated_user_specified_value, simulated_random_number);
+
+  let x = 4;
+  // let equal_to_x = |z| z == x;
+  let y = 4;
+  // assert!(equal_to_x(y));
+  let display = |z| {
+    println!("show 1: {}", z);
+    println!("show 2: {}", x);
+  };
+  let y = 40;
+  display(y);
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
--- rust/rust-book/run.sh
+++ rust/rust-book/run.sh
@@ -1,3 +1,3 @@
-run () {
+runrs () {
   rustc $1.rs -o $1.out && ./$1.out
 }
