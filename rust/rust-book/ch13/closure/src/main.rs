4a2602a10a9acf4d665d36442fef80eb71d1c99e
Peter Lau <superpeterlau@outlook.com>
2020-09-25 16:46:55 +0800

    rust: rust-book ch13 closure : use closure in struct

--- rust/rust-book/ch13/closure/src/main.rs
+++ rust/rust-book/ch13/closure/src/main.rs
@@ -1,6 +1,37 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+    {
+      calculation: T,
+      value: Option<u32>,
+    }
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+    {
+      fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+          calculation,
+          value: None,
+        }
+      }
+
+      fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+          Some(v) => v,
+          None => {
+            let v = (self.calculation)(arg);
+            self.value = Some(v);
+            v
+          }
+        }
+      }
+    }
+
 fn main() {
   // 实际要从前端获得
   let simulated_user_specified_value = 10;
@@ -20,20 +51,20 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 fn generate_workout(intensity: u32, randome_number: u32) {
   // let expensive_result = simulated_expensive_calculation(intensity);
 
-  let expensive_closure = |num| {
+  let mut expensive_result = Cacher::new (|num| {
     println!("Calculating slowly ...");
     thread::sleep(Duration::from_secs(2));
     num
-  };
+  });
 
   if intensity < 25 {
     println!(
       "Today, do {} pushups!",
-      expensive_closure(intensity)
+      expensive_result.value(intensity)
     );
     println!(
       "Next, do {} situps!",
-      expensive_closure(intensity)
+      expensive_result.value(intensity)
     );
   } else {
     if randome_number == 3 {
@@ -41,8 +72,21 @@ fn generate_workout(intensity: u32, randome_number: u32) {
     } else {
       println!(
         "Today, run for {} minutes!",
-        expensive_closure(intensity)
+        expensive_result.value(intensity)
       );
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+    assert_eq!(v2, 2);
   }
 }
