be56aa74902f707a61b68a9f47264a877483e1fe
Peter Lau <superpeterlau@outlook.com>
2020-10-14 09:47:37 +0800

    elisp tour

---
+++ elisp/learn.el
@@ -0,0 +1,11 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+(- 90 (* 9 9))
+
+(set 'flowers '(rose violet daisy buttercup))
+(flowers)
+
+(setq flowers '(rose violet daisy buttercup))
+
+
