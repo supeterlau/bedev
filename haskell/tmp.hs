fd1405aef3b29af95021b413687bd8d62eea738e
Peter Lau <superpeterlau@outlook.com>
2020-12-29 23:58:50 +0800

    update haskell tmp

--- haskell/tmp.hs
+++ haskell/tmp.hs
@@ -1,4 +1,18 @@
-main = do
-  putStrLn "What' your name"
-  name <- getLine
-  putStrLn ("Hey " ++ name ++ ", you rock!")
+-- main = do
+--   putStrLn "What' your name"
+--   name <- getLine
+--   putStrLn ("Hey " ++ name ++ ", you rock!")
+
+class DummyMonad m where
+  (>>>=) :: m a -> (a -> m b) -> m b
+
+instance DummyMonad Maybe where
+  Nothing >>>= func = Nothing
+  Just val >>>= func = func val
+
+half x = if even x
+          then Just (x `div` 2)
+          else Nothing
+
+-- main = do
+--   putStrLn (Just(10) >>>= half)
