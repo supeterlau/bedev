6ac92be97686318705d217bddf27bd64f4565f8b
Peter Lau <superpeterlau@outlook.com>
2021-03-25 23:41:10 +0800

    shell: make macapp

---
+++ shell/gen_macapp.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+#
+# gen_macapp.sh
+# Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+CLI=$(realpath $1)
+APPNAME=${2:-$(basename "${CLI}" '.sh')};
+APP="/Applications/${APPNAME}.app";
+DIR="${APP}/Contents/MacOS";
+
+if [ -a "${APP}" ]; then
+	echo "${APP} already exists :(";
+	exit 1;
+fi;
+
+mkdir -p "${DIR}";
+
+APP_EXEC=${DIR}/${APPNAME}
+ln -Fs "${CLI}" "${APP_EXEC}";
+[[ ! -x ${APP} ]] && chmod +x "${APP_EXEC}";
+
+echo "${APP}";
