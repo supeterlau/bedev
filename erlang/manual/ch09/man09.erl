aa29f1be8b28d929ce5aa09395715deaa78af0af
Peter Lau <superpeterlau@outlook.com>
2021-04-18 01:00:09 +0800

    erlang: manual ch09

---
+++ erlang/manual/ch09/man09.erl
@@ -0,0 +1,17 @@
+-module(man09).
+
+-export([start/0]).
+
+-define(ADD(X, Y), X + Y).
+-define(message, "Yeah").
+-define(CALL(Func), io:format("Call ~s: ~w~n", [??Func, Func])).
+
+fakeFunction(M, N) ->
+  M + N.
+
+start() ->
+  io:format("machine name: ~w~n", [?MACHINE]),
+  io:format("otp: ~w~n", [?OTP_RELEASE]),
+  io:format("Say: ~s~n", [?message]),
+  ?CALL(fakeFunction(100, 200)),
+  io:format("ADD(10, 100): ~B~n", [?ADD(10, 100)]).
