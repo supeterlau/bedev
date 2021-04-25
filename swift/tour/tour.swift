7232fdfd583262f59fe21787e2cc7d276855e9ee
Peter Lau <superpeterlau@outlook.com>
2021-04-25 11:10:19 +0800

    swfit: tour taste

---
+++ swift/tour/tour.swift
@@ -0,0 +1,20 @@
+print("OK")
+// print('O')
+
+let appleNumber = 30
+
+print("I got \(appleNumber) apples.\n")
+
+var shoppingList = ["milk", "apple", "egg"]
+print(shoppingList.append("coffee"))
+print(shoppingList)
+
+print("We will buy \(shoppingList[0]) first")
+
+// need explicit type
+
+// var emptyList = []
+// print(emptyList)
+
+
+print("THE END")
