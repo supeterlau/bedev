cc2ec6a8b32381795c276e154b8484bc11d874d7
Peter Lau <superpeterlau@outlook.com>
2021-05-17 14:14:32 +0800

    fsharp: tour

---
+++ fsharp/start/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Everything in this section is true, but only part of the truth"
+    0 // return an integer exit code
