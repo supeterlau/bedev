3f674973cc7b70c0dfa76a881f052bde31374b68
Peter Lau <superpeterlau@outlook.com>
2021-06-15 16:21:12 +0800

    rust: rustbook ch15 Box<T>

---
+++ rust/rust-book/ch15/01.rs
@@ -0,0 +1,10 @@
+//
+// 01.rs
+// Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+// Distributed under terms of the MIT license.
+//
+
+fn main() {
+	let b = Box::new(5000);
+	println!("b is {}", b);
+}
