1b0d14d8eadfbb4318a2908f3f2816e3e88a8402
Peter Lau <superpeterlau@outlook.com>
2021-09-15 19:24:20 +0800

    try elixir monad

--- elixir/example/monad/app0.ex
+++ elixir/example/monad/app0.ex
@@ -21,12 +21,83 @@ defmodule App.Maybe do
   def nothing, do: __MODULE__ |> struct(nothing: true)
 end
 
-defprotocol Functor do
+defprotocol App.Functor do
   @spec fmap(t, (term -> term)) :: t
   def fmap(functor, fun)
 end
-defimpl Functor, for: App.Maybe do
 
+defimpl App.Functor, for: App.Maybe do
+  def fmap(%{nothing: true} = f, _), do: f
+  def fmap(%{just: a}, fun) do
+    case val = fun |> apply([a]) do
+      %{nothing: true} -> val
+      _ -> val |> App.Maybe.just()
+    end
+  end
+end
+
+defmodule App.Magic do
+  def call(stat, val) do
+    IO.inspect(stat, label: "App.Magic")
+    IO.inspect(val, label: "App.Magic")
+  end
+end
+
+defprotocol App.Applicative do
+  @spec apply(t, App.Functor.t) :: t
+  def apply(fun, f)
+end
+
+defimpl App.Applicative, for: App.Maybe do
+  def apply(%{nothing: true} = f, _), do: f
+  def apply(%{just: fun}, f) do
+    f |> App.Functor.fmap(fun)
+  end
+end
+
+defprotocol App.Monad do
+  @spec bind(t, (term -> t)) :: t
+  def bind(m, fun)
+end
+
+defimpl App.Monad, for: App.Maybe do
+  def bind(%{nothing: true} = f, _), do: f
+  def bind(%{just: v}, fun) do
+    fun |> apply([v])
+  end
 end
 # App.Maybe.just(3)
 # App.Maybe.nothing()
+
+add2 = fn x -> x + 2 end
+to_nothing = fn _ -> App.Maybe.nothing() end
+mul3 = fn x -> x * 3 end
+
+App.Maybe.just(5)
+|> App.Functor.fmap(add2)
+|> IO.inspect()
+
+App.Maybe.just(5)
+|> App.Functor.fmap(add2)
+|> App.Functor.fmap(mul3)
+|> IO.inspect()
+
+App.Maybe.just(5)
+|> App.Functor.fmap(add2)
+|> App.Functor.fmap(to_nothing)
+|> App.Functor.fmap(mul3)
+|> IO.inspect()
+
+call_magic = fn file ->
+  case File.stat(file) do
+    {:ok, s} ->
+      App.Maybe.just(&(&1 |> App.Magic.call(s)))
+    {:error, _} ->
+      App.Maybe.nothing()
+  end
+end
+
+# call_magic.("not_found.txt")
+call_magic.("app0.js")
+|> App.Applicative.apply(App.Maybe.just(5))
+|> IO.inspect()
