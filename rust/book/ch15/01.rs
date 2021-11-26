6a71eda72777d2978fbfa1d65ccdd0f1fc8b606f
Peter Lau <superpeterlau@outlook.com>
2021-11-27 02:15:55 +0800

    rust: rustbook ch15 15.1

---
+++ rust/book/ch15/01.rs
@@ -0,0 +1,24 @@
+//
+// 01.rs
+// Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+// Distributed under terms of the MIT license.
+//
+
+
+enum List {
+  // Cons(i32, List), // OCaml style
+  Cons(i32, Box<List>),
+  Nil,
+}
+
+// use crate::List::{Cons, Nil};
+
+fn main() {
+	let b = Box::new(5000);
+	println!("b is {}", b);
+
+
+  // let list = Cons(1, Cons(2, Cons(3, Nil)));
+  // 使用 enum
+  let list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))));
+}
