0a8bcad5223dbd687c5b8401cb6e7bd3950fe8c1
Peter Lau <superpeterlau@outlook.com>
2021-12-02 16:19:43 +0800

    elixir advent of code 2021: day02

---
+++ elixir/advent/2021/day02/sol.exs
@@ -0,0 +1,41 @@
+defmodule Advent.Day02 do
+  def parse_input() do
+     File.read!('./input.txt') |> String.split("\n") |> Enum.reject(&(&1==""))
+  end
+
+  def p1() do
+    {hp, depth} =  
+      parse_input() 
+      |> Enum.reduce({0, 0}, fn 
+        "forward " <> move, {hp, depth} -> 
+         {hp+String.to_integer(move), depth}
+        "up " <> move, {hp, depth} -> 
+          {hp, depth-String.to_integer(move)}
+        "down " <> move, {hp, depth} -> 
+          {hp, depth+String.to_integer(move)}
+     end)
+     |> IO.inspect()
+    IO.inspect(hp*depth)
+  end
+
+  def exec() do
+    {hp, depth, _} =  
+      parse_input() 
+      |> Enum.reduce({0, 0, 0}, fn 
+        "forward " <> move, {hp, depth, aim} -> 
+          {hp+String.to_integer(move), depth+String.to_integer(move)*aim, aim}
+        "up " <> move, {hp, depth, aim} -> 
+          # {hp, depth-String.to_integer(move), aim-String.to_integer(move)}
+          {hp, depth, aim-String.to_integer(move)}
+        "down " <> move, {hp, depth, aim} -> 
+          # {hp, depth+String.to_integer(move), aim+String.to_integer(move)}
+          {hp, depth, aim+String.to_integer(move)}
+     end)
+     |> IO.inspect()
+    IO.inspect(hp*depth)
+  end
+end
+
+# horizontal position
+
+Advent.Day02.exec()
