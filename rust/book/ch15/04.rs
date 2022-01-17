2819d4fa988c33bdc77bb84822f58c7bf81934ed
Peter Lau <superpeterlau@outlook.com>
2022-01-17 23:39:14 +0800

    Rust Book ch15 04

---
+++ rust/book/ch15/04.rs
@@ -0,0 +1,37 @@
+/*
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+ */
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main(){
+    /*
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+
+    let _b = Cons(3, Box::new(a));
+    let _c = Cons(4, Box::new(a));
+     */
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+    println!("count after creating a: {}", Rc::strong_count(&a));
+
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating b: {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c: {}", Rc::strong_count(&a))
+    }
+    println!("count after c out of scope: {}", Rc::strong_count(&a))
+}
