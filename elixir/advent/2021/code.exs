98e00b298ebe081023152b82d351c07dfd2a49f2
Peter Lau <superpeterlau@outlook.com>
2022-05-25 21:28:08 +0800

    elixir/advent/2021/code.exs

--- elixir/advent/2021/code.exs
+++ elixir/advent/2021/code.exs
@@ -1,7 +1,16 @@
 defmodule Advent.Day do
+
+  def log(item, label) do
+    IO.inspect(item, charlists: :list, label: label)
+  end
+
+  def log(item) do
+    IO.inspect(item, charlists: :list)
+  end
    
   def parse_input() do
-     File.read!('./input.txt') |> String.split(~r{\n}, trim: true)
+     File.read!('./input1.txt') |> String.split(~r{\n}, trim: true)
+     # File.read!('./input.txt') |> String.split(~r{\n}, trim: true)
   end
 
   def exec() do
@@ -10,4 +19,4 @@ defmodule Advent.Day do
 end
 
 Advent.Day.exec()
-
+|> Advent.Day.log()
