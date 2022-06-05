368695e021936c780f4414f63d045d075ef8ea11
Peter Lau <superpeterlau@outlook.com>
2022-06-05 16:05:28 +0800

    java/quick/compile.sh

--- java/quick/compile.sh
+++ java/quick/compile.sh
@@ -2,6 +2,6 @@ runjava() {
   name=$1
   demo_name="demo/Demo${name}.java"
   class="Demo${name}"
-  javac --enable-preview --release 15 $demo_name
+  javac -Xlint:preview --enable-preview --release 15 $demo_name
   java --enable-preview demo.$class
 }
