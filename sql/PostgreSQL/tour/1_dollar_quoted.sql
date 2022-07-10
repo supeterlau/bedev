d276a05fe4ed80cf8cff841293f9e12a75c90787
Peter Lau <superpeterlau@outlook.com>
2022-07-10 22:42:39 +0800

    sql/PostgreSQL/tour/export_csv.sql

--- sql/PostgreSQL/tour/1_dollar_quoted.sql
+++
@@ -1,8 +0,0 @@
-do 
-'declare
-   film_count integer;
-begin 
-   select count(*) into film_count
-   from "Users";
-   raise notice ''The number of films: %'', film_count;
-end;';
--- sql/PostgreSQL/tour/export_csv.sql
+++
@@ -1,2 +0,0 @@
--- \copy (select * from "Users") to '/home/db/users.csv' with csv
-\copy (select * from "Users") to '/home/db/users.csv' DELIMITER ',' CSV HEADER;
