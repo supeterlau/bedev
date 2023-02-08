8816b609286531ba0e82eeba7377c987dfc732ad
Peter Lau <superpeterlau@outlook.com>
2023-02-08 18:52:53 +0800

    trace: elixir/tour/mix_and_otp/demo_server/lib/demo_server/application.ex

---
+++ elixir/tour/mix_and_otp/demo_server/lib/demo_server/application.ex
@@ -0,0 +1,19 @@
+defmodule DemoServer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: DemoServer.Worker.start_link(arg)
+      # {DemoServer.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: DemoServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
