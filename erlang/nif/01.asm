f29f5e66bcc457911c2fe0de21d8466f003bd8b6
Peter Lau <superpeterlau@outlook.com>
2023-02-14 22:00:15 +0800

    trace: erlang/nif/01.asm

---
+++ erlang/nif/01.asm
@@ -0,0 +1,16 @@
+        global    _main
+				
+				section   .text
+_main:
+        mov rax, 0x02000004
+				mov rdi, 1
+				mov rsi, message
+				mov rdx, 13 ; message bytes_count
+				syscall
+				mov rax, 0x02000001
+				xor rdi, rdi
+				syscall
+
+				section    .data
+message:
+				db	'Hello, World', 10
