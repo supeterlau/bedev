5c7f79670afdb830488f4e80e67126b439ecac2a
Peter Lau <superpeterlau@outlook.com>
2023-03-25 16:04:22 +0800

    trace: java/solid/Vehicle.java

---
+++ java/solid/Vehicle.java
@@ -0,0 +1,19 @@
+/*
+ * Vehicle.java
+ * Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class Vehicle {
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String c) {
+		this.color = c;
+	}
+}
+
