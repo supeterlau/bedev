5e5e563e8c697fdf41e8a9af0256db4327ec07c8
Peter Lau <superpeterlau@outlook.com>
2023-04-11 21:08:46 +0800

    trace: kotlin/start/k00.kts

---
+++ kotlin/start/k00.kts
@@ -0,0 +1,13 @@
+// https://www.tutorialspoint.com/kotlin/kotlin_basic_syntax.htm
+//
+
+// package org.kotlindev
+
+fun main(args: Array<String>) {
+	print("Kotlin program")
+	var zen: String = "Beautiful is better than ugly."
+	println("$zen")
+	print("Kotlin program")
+}
+
+main(arrayOf<String>())
